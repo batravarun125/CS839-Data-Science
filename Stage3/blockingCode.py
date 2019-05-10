fa398dff9ea49967b407600e262198060dd36270
Varun Batra <batravarun125@yahoo.co.in>
2019-05-10 16:33:41 -0500

    Stage-3 done

--- Stage3/blockingCode.py
+++ Stage3/blockingCode.py
@@ -4,21 +4,6 @@ import pandas as pd
 amazon=pd.read_csv('tableA')
 goodreads=pd.read_csv('tableB')
 temp_set=[]
-
-
-def check_jaccard(string1,string2):
-    setA=[]
-    setB=[]
-    count=0
-    for i in range(0, len(string1) - 2):
-        setA.append(string1[i : i + 3])
-    for i in range(0, len(string2) - 2):
-        setB.append(string2[i : i + 3])
-    for item in setA:
-        if item in setB:
-            count = count + 1
-    den = len(string1) + len(string2) - count
-    return float(count) / den
     
 
 if __name__ == '__main__':
@@ -27,7 +12,20 @@ if __name__ == '__main__':
         stringA=str(amazon.iloc[candidate_set.iloc[i,0],1]).upper()
         # print(str(goodreads.iloc[candidate_set.iloc[i,1],1]).upper())
         stringB=str(goodreads.iloc[candidate_set.iloc[i,1],1]).upper()
-        if check_jaccard('##' + stringA + '##','##' + stringB + '##')<0.2:
+        stringA = '##' + stringA + '##'
+        stringB = '##' + stringB + '##'
+        setA=[]
+        setB=[]
+        count=0
+        for j in range(0, len(stringA) - 2):
+            setA.append(stringA[j : j + 3])
+        for j in range(0, len(stringB) - 2):
+            setB.append(stringB[j : j + 3])
+        for item in setA:
+            if item in setB:
+                count = count + 1
+        den = len(stringA) + len(stringB) - count
+        if float(count) / den < 0.2:
             temp_set.append(i)
     candidate_set.drop(temp_set, inplace=True)
     print(len(candidate_set))
