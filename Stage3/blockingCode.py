1a0d2f2f669c74ffc16cc069ec89fa1dd36ca171
Varun Batra <batravarun125@yahoo.co.in>
2019-05-10 10:02:52 -0500

    Stage-3 done

--- Stage3/blockingCode.py
+++ Stage3/blockingCode.py
@@ -1,34 +1,35 @@
 import numpy as np
 import pandas as pd
 
-
-def check_jaccard(string1,string2):
-    string1 ='##' + string1 + '##'
-    string2 ='##' + string2 + '##'
-    setA=[]
-    setB=[]
-    for j in range(0,len(string1)-2):
-        setA.append(string1[j:j+3])
-    for j in range(0,len(string2)-2):
-        setB.append(string2[j:j+3])
-    count=0
-    for j in setA:
-        if j in setB:
-            count = count + 1
-    return float(count)/(len(string1)+len(string2)-count)
-    
-
-amazon=pd.read_csv('/Users/varunbatra/Downloads/tableA')
-goodreads=pd.read_csv('/Users/varunbatra/Downloads/tableB')
-candidate_set=pd.read_csv('/Users/varunbatra/Downloads/stage3_1_apply_rules_ds')
+amazon=pd.read_csv('tableA')
+goodreads=pd.read_csv('tableB')
+candidate_set=pd.read_csv('stage3_1_apply_rules_ds')
 #print(len(candidate_set))
 final_set=[]
-for i in range(0,len(candidate_set)):
-    stringA=str(amazon.iloc[candidate_set.iloc[i,0],1]).upper()
-    # print(str(goodreads.iloc[candidate_set.iloc[i,1],1]).upper())
-    stringB=str(goodreads.iloc[candidate_set.iloc[i,1],1]).upper()
-    if check_jaccard(stringA,stringB)<0.2:
-        final_set.append(i)
-candidate_set.drop(final_set,inplace=True)
-print(len(candidate_set))
-candidate_set.to_csv('/Users/varunbatra/Downloads/candidate_blocking.csv',sep=',',header=True,index=False)
+
+
+def check_jaccard(string1,string2):
+    setA=[]
+    setB=[]
+    count=0
+    for i in range(0, len(string1) - 2):
+        setA.append(string1[i : i + 3])
+    for i in range(0, len(string2) - 2):
+        setB.append(string2[i : i + 3])
+    for item in setA:
+        if item in setB:
+            count = count + 1
+    return float(count) / (len(string1) + len(string2) - count)
+    
+
+if __name__ == '__main__':
+
+    for i in range(0,len(candidate_set)):
+        stringA=str(amazon.iloc[candidate_set.iloc[i,0],1]).upper()
+        # print(str(goodreads.iloc[candidate_set.iloc[i,1],1]).upper())
+        stringB=str(goodreads.iloc[candidate_set.iloc[i,1],1]).upper()
+        if check_jaccard('##' + stringA + '##','##' + stringB + '##')<0.2:
+            final_set.append(i)
+    candidate_set.drop(final_set,inplace=True)
+    print(len(candidate_set))
+    candidate_set.to_csv('candidate_blocking.csv',sep=',',header=True,index=False)
